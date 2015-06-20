8149be3c9d1da7353c971c3ee777516c7c8e13f9
Valentin Funk <funk.valentin@gmail.com>
2015-06-20 16:40:37 +0200

    Add Blackvoid/Deathrun support

---
+++ lua/mapvote/server/sv_drintegration.lua
@@ -0,0 +1,24 @@
+if not MAPVOTE.BVDeathrun or not MAPVOTE.BVDeathrun.Enable then
+	return
+end
+
+hook.Add( "InitPostEntity", "KMVSupportBlackvoidDR", function( ) 
+	GAMEMODE.MapVoteCheck = function( )
+	end
+end )
+
+hook.Add( "CanStartRound", "KMVPreventRoundStart", function( )
+	if MAVOTE:GetState( ) != "STATE_NOVOTE" then
+		return false
+	end
+end )
+
+hook.Add( "KMapVoteMapExtension", "KMVMAkeSureRoundStarts", function( )
+	timer.Simple( 0.5, function( )
+		GAMEMODE:RoundStatusCheck( )
+	end )
+end )
+
+hook.Add( "OnRoundEnd", "KMapVoteCountRoundsDR", function( )
+	MAPVOTE.CustomRoundEnded( "BVDeathrun" )
+end )
